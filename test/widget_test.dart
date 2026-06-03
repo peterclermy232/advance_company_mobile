0a048cb10aca68b2f006144e7b7dcadc6f13f255
Peter Atito <peterclermy232@gmail.com>
2026-06-03 14:53:25 +0300

    test: fix app import to match package name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,4 +1,4 @@
-import 'package:advance_company/app.dart';
+import 'package:saccoapp/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
